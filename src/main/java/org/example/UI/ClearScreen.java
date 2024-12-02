4f4325386b4f86b67d518579c73f722eb659a997
Guilherme Santana <guiguilsantana@gmail.com>
2024-12-01 22:57:44 -0300

    feat: "Creating ClearScreen"

---
+++ src/main/java/org/example/UI/ClearScreen.java
@@ -0,0 +1,14 @@
+package org.example.UI;
+
+public class ClearScreen {
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    public static void centralizarTexto(String texto) {
+        int larguraTerminal = 80; // Largura padrão do terminal
+        int espacosAntes = (larguraTerminal - texto.length()) / 2;
+        String espacos = " ".repeat(espacosAntes);
+        System.out.println(espacos + texto);
+    }
+}
